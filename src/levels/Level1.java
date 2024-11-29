1e06c6cb71b79f01898a75bcd66c1d2f4dfb9b43
janelson <janelsonfucapi@gmail.com>
2024-11-29 11:30:48 -0400

    Criação do Level 1

--- src/levels/Level1.java
+++ src/levels/Level1.java
@@ -6,4 +6,18 @@ import entities.Platform;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class Level1 extends JPanel implements Runnable {
+    private JFrame frame;
+    private Player player;
+    private ArrayList<Platform> platforms = new ArrayList<>();
+    private int gravity = 5;
+    private boolean gameOver = false;
+
+    public Level1(JFrame frame) {
+        this.frame;
+    }
+    plataforms.add(new Platform(0,500,800))
+}
